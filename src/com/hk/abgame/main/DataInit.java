56953bf80edaac932aa5945ad8ff57d268157456
“krisxia0506” <xiajiayi0506@gmail.com>
2022-06-28 11:05:35 +0800

    1

---
+++ src/com/hk/abgame/main/DataInit.java
@@ -0,0 +1,15 @@
+package com.hk.abgame.main;
+
+import com.hk.abgame.bean.Login;
+
+/**
+ * Created on 2022-06-28 10:15
+ *
+ * @author Xia Jiayi
+ */
+public class DataInit {
+    public static Login login = new Login();
+    static {
+
+    }
+}
---
+++ src/com/hk/abgame/util/XMLFReader.java
@@ -0,0 +1,9 @@
+package com.hk.abgame.util;
+
+/**
+ * Created on 2022-06-28 10:41
+ *
+ * @author Xia Jiayi
+ */
+public class XMLFReader {
+}
