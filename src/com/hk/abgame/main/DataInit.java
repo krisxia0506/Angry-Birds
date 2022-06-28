0c4587c60e744b121e891ef3cac5e45884fd20ba
“krisxia0506” <xiajiayi0506@gmail.com>
2022-06-28 11:38:01 +0800

    1

--- src/com/hk/abgame/main/DataInit.java
+++ src/com/hk/abgame/main/DataInit.java
@@ -1,6 +1,10 @@
 package com.hk.abgame.main;
 
 import com.hk.abgame.bean.Login;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
+import javax.swing.text.Document;
+import java.io.File;
 
 /**
  * Created on 2022-06-28 10:15
@@ -10,6 +14,15 @@ import com.hk.abgame.bean.Login;
 public class DataInit {
     public static Login login = new Login();
     static {
+        SAXReader reader = new SAXReader();
+        File file = new File("DataInit.xml");
+
+        try {
+            Document document = (Document) reader.read(file);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
--- src/com/hk/abgame/util/InputHelper.java
+++ src/com/hk/abgame/util/InputHelper.java
@@ -1,9 +1,15 @@
 package com.hk.abgame.util;
 
+import java.util.Scanner;
+
 /**
  * Created on 2022-06-28 10:40
  *
  * @author Xia Jiayi
  */
 public class InputHelper {
+    public static int getInt() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
 }
