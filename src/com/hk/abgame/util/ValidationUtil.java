974a584161dde7a468e26b454eb81b8b73fada8c
夏佳怡 <xiajiayi0506@163.com>
2022-07-01 10:36:31 +0800

    V1.0.6 创建数据验证类ValidationUtil和字符串工具类StringUtil 数据验证类ValidationUtil主要包含正则表达式静态常量 字符串工具类StringUtil调用数据验证类的正则表达式 重载InputHelper.getString方法，使用正则表达式和提示信息为形参，可对不同的输入内容进行对应的检查和提示

---
+++ src/com/hk/abgame/util/ValidationUtil.java
@@ -0,0 +1,19 @@
+package com.hk.abgame.util;
+
+/**
+ * Created on 2022-07-01 9:47
+ * 用于验证的工具类
+ * @author Xia Jiayi
+ */
+public class ValidationUtil {
+
+    //登录名:不能全部由数字组成，必须要有字母
+    public static final String CHKLOGINNANE = "^(?![0-9]+$)[a-zA-Z0-9]{1,16}$";
+    //密码:有数字和字母，长度不小于六位
+    public static final String CHKPASSIORD = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9a-zA-Z]{6,16}$";
+    //性别:男或者女male female
+    public static final String CHKSEX = "^['男','女']$";
+    //年龄:必须是正整数，1-99
+    public static final String CHKAGE = "^[1-9][0-9]?$";
+
+}
